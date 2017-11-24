8660e5be195edca964612f8e909b2339119a3eeb
tiantinjinfeng <1835458378@qq.com>
2017-11-24 12:55:37 +0800

    初始化
    初始化

---
+++ pc/js/index.js
@@ -0,0 +1,129 @@
+//文档就绪
+$(function(){
+	
+	//获得回到顶部按钮对象
+	var topcontrol=$('#topcontrol');
+	//获得导航条对象
+	var nav = $('.nav_wrap');
+	//导航栏相对网页原点的位置
+	var navPos = nav.offset().top;
+	//导航栏本身高度
+	var navHeight = nav.outerHeight();
+	
+	//滚动条事件
+	$(window).scroll(function(e){
+		
+		//获得滚动条位置
+		var sTop = $(window).scrollTop();
+		
+		// 只有滚动条超过200px才有必要显示回到顶部按钮
+		if(sTop >=200){
+			topcontrol.fadeIn(300);
+		} else {
+			topcontrol.fadeOut(300);
+		}
+		
+		//自动给导航条添加fix
+		if(sTop >= navPos){
+			if(!(nav.hasClass('fix'))){
+				nav.addClass('fix');
+				$('.banner').css('margin-bottom',navHeight);//banner下面空出等高占位
+			}
+		} else {
+			if((nav.hasClass('fix'))){
+				nav.removeClass('fix');
+				$('.banner').css('margin-bottom',0);
+			}
+		}
+		
+		/*滚动监听高亮导航*/
+		
+		//海量正版免费下载
+		var introPos={
+			start:$('#post_intro').offset().top - navHeight,
+			end:$('#post_intro').offset().top+$('#post_intro').outerHeight() - navHeight
+		}
+		
+		//判断滚动条范围
+		if(sTop >=  introPos.start && sTop < introPos.end){
+			if(!($('.intro').hasClass('active'))){
+				$('.intro').addClass('active');//高亮
+			}			
+		} else {
+			if(($('.intro').hasClass('active'))){
+				$('.intro').removeClass('active');//高亮
+			}
+		}
+		
+		//无需苹果账号
+		var usagePos={
+			start:$('#post_usage').offset().top - navHeight,
+			end:$('#post_usage').offset().top + $('#post_usage').outerHeight() - navHeight
+		}
+		
+		//判断滚动条范围
+		if(sTop >=  usagePos.start && sTop < usagePos.end){
+			if(!($('.usage').hasClass('active'))){
+				$('.usage').addClass('active');//高亮
+			}			
+		} else {
+			if(($('.usage').hasClass('active'))){
+				$('.usage').removeClass('active');//高亮
+			}
+		}
+		
+		//手机瘦身
+		var choicePos={
+			start:$('#choiceness').offset().top - navHeight,
+			end:$('#choiceness').offset().top+$('#choiceness').outerHeight() - navHeight
+		}
+		
+		//判断滚动条范围
+		if(sTop >=  choicePos.start && sTop < choicePos.end){
+			if(!($('.choice').hasClass('active'))){
+				$('.choice').addClass('active');//高亮
+			}			
+		} else {
+			if(($('.choice').hasClass('active'))){
+				$('.choice').removeClass('active');//高亮
+			}
+		}
+	});
+	
+	
+	//回到顶部按钮单击事件
+	topcontrol.click(function(){
+		//滚动条过渡到顶部
+		$('html,body').animate({scrollTop:0},1000);
+	})
+		
+	//网页导航锚点过渡
+	$('.nav_wrap a').click(function(){
+		
+		//获得对应区域相对于网页原点的位置
+		var objTop = $(this.hash).offset().top - navHeight + 7;
+		//滚动条过渡到顶部
+		$('html,body').animate({scrollTop:objTop},1000);
+		return false;
+		
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
